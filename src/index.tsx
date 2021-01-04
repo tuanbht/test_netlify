54e69dcfeecde69799a71953d0499f84976b1da4
Cuong Nguyen and Tuan Bui <cuong.nguyen+tuan.bui@eastagile.com>
2021-01-04 17:51:39 +0700

    [r] remove https redirect component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HttpsRedirect from './HttpsRedirect';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HttpsRedirect>
-      <App />
-    </HttpsRedirect>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
